d741f3280259c8ae07a80c1a1e5152ed2341d71f
Fadhil <fadhilahdwi.a@gmail.com>
2024-10-09 21:09:58 +0700

    update widget bottom navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:coffee_shop_mobile_apps/utils/index.dart';
+import 'package:coffee_shop_mobile_apps/widgets/bottom_navigation.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -29,72 +29,9 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(249, 249, 249, 1),
       body: _pages[_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              spreadRadius: 1,
-              blurRadius: 6,
-              offset: Offset(0, -1),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            selectedFontSize: 0,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: buildSvgIcon(
-                    'home_outline',
-                    _selectedIndex == 0
-                        ? const Color.fromRGBO(198, 124, 78, 1)
-                        : const Color.fromRGBO(162, 162, 162, 1),
-                    _selectedIndex == 0),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: buildSvgIcon(
-                    'heart_outline',
-                    _selectedIndex == 1
-                        ? const Color.fromRGBO(198, 124, 78, 1)
-                        : const Color.fromRGBO(162, 162, 162, 1),
-                    _selectedIndex == 1),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: buildSvgIcon(
-                    'cart_outline',
-                    _selectedIndex == 2
-                        ? const Color.fromRGBO(198, 124, 78, 1)
-                        : const Color.fromRGBO(162, 162, 162, 1),
-                    _selectedIndex == 2),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: buildSvgIcon(
-                    'bell_outline',
-                    _selectedIndex == 3
-                        ? const Color.fromRGBO(198, 124, 78, 1)
-                        : const Color.fromRGBO(162, 162, 162, 1),
-                    _selectedIndex == 3),
-                label: '',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          ),
-        ),
+      bottomNavigationBar: BottomNavigation(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
---
+++ lib/widgets/bottom_navigation.dart
@@ -0,0 +1,88 @@
+import 'package:coffee_shop_mobile_apps/utils/index.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const BottomNavigation({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 6,
+            offset: const Offset(0, -1),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 0,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: buildSvgIcon(
+                'home_outline',
+                selectedIndex == 0
+                    ? const Color.fromRGBO(198, 124, 78, 1)
+                    : const Color.fromRGBO(162, 162, 162, 1),
+                selectedIndex == 0,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: buildSvgIcon(
+                'heart_outline',
+                selectedIndex == 1
+                    ? const Color.fromRGBO(198, 124, 78, 1)
+                    : const Color.fromRGBO(162, 162, 162, 1),
+                selectedIndex == 1,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: buildSvgIcon(
+                'cart_outline',
+                selectedIndex == 2
+                    ? const Color.fromRGBO(198, 124, 78, 1)
+                    : const Color.fromRGBO(162, 162, 162, 1),
+                selectedIndex == 2,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: buildSvgIcon(
+                'bell_outline',
+                selectedIndex == 3
+                    ? const Color.fromRGBO(198, 124, 78, 1)
+                    : const Color.fromRGBO(162, 162, 162, 1),
+                selectedIndex == 3,
+              ),
+              label: '',
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+        ),
+      ),
+    );
+  }
+}
